747a861608bcabc588258afb8172a3889c7e5a0b
Cumar Yusuf <omar221@windowslive.com>
2015-04-05 12:11:22 -0600

    refurbished music module

--- stge7/music.c
+++ stge7/music.c
@@ -11,33 +11,10 @@ const int song[] =  {43, 20, 0, 20, 44, 20, 0 , 20, 43 , 20, 0 , 20 ,42 , 10, 41
 int index;
 
 void start_music(){
-    int i;
     index = 0;
-    /*
     set_tone(0, song[index]);
     enable_channel(0, 0, 1);
-    set_volume(0,11);
-    */
-    i = 0;
-    while(i < 255 ){
-         set_tone(1,100);
-        set_tone(0,i);
-       
-        enable_channel(0, 0, 1);
-        enable_channel(1, 0, 1);
-        set_volume(1,0);
-        set_volume(0,11);
-       
-         printf ("%d\n", i);
-        while(!Cconis()){
-            ;
-        }
-       Cnecin();
-       set_volume(1,11);
-       i++;
-    }
-    
-    
+    set_volume(0,11);    
 }
 
 int update_music(UINT32 time_elapsed){
